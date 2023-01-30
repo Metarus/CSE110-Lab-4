94ef648d94203f10dee256f881b46f68aac8e70b
Akshat <a1mittal@ucsd.edu>
2023-01-30 15:00:32 -0800

    Fuck

--- app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
+++ app/src/main/java/edu/ucsd/cse110/lab4/ProfileActivity.java
@@ -3,16 +3,12 @@ package edu.ucsd.cse110.lab4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onLaunchProfileClicked(View view) {
+        setContentView(R.layout.activity_profile);
     }
 }
